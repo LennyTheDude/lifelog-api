5900bfc4b5dafae1851e1061b15a3309bef0d783
Leo A <lennythedude27@gmail.com>
2023-03-20 21:38:43 +0530

    Testing pipeline

--- dist/lifelog-api.js
+++ dist/lifelog-api.js
@@ -7,9 +7,9 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('<h2>API for network.lennythedude.com will be here shortly</h2>');
+    res.send('<h2>API for lifelog.lennythedude.com will be here shortly</h2>');
 });
 app.listen(port, () => {
-    return console.log(`The network API is listening on port ${port}`);
+    return console.log(`The Lifelog API is listening on port ${port}`);
 });
 //# sourceMappingURL=lifelog-api.js.map

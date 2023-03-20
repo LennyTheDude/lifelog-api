bfa139c2ae4d05b5d1d24ca7a026723c0aa0c63c
Leo A <lennythedude27@gmail.com>
2023-03-20 18:45:22 +0530

    Added Github Workflow + text change to test

--- src/lifelog-api.ts
+++ src/lifelog-api.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h2>API for network.lennythedude.com will be here shortly</h2>');
+  res.send('<h2>API for lifelog.lennythedude.com will be here shortly</h2>');
 });
 
 app.listen(port, () => {
-  return console.log(`The network API is listening on port ${port}`);
+  return console.log(`The Lifelog API is listening on port ${port}`);
 })

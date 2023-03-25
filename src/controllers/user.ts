b68bc87891f9a20d20a4f16b1cd61c374b0f8c00
Leo A <lennythedude27@gmail.com>
2023-03-25 23:40:45 +0530

    Done Login/Signup/GetAllUsers routes

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,7 +5,9 @@ import { Connect, Query } from '../config/psql';
 import IUser from '../interfaces/user';
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
-    console.log("User authorized");
+    // @TODO: validate a token coming in request
+
+    console.log("Token validated.");
 
     return res.status(200).json({
         message: "Authorized"
@@ -13,11 +15,13 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
 }
 
 const register = (req: Request, res: Response, next: NextFunction) => {
-    let { username, password } = req.body;
+    console.log('req.body', req.body);
+
+    const { username, password } = req.body;
 
     bcryptjs.hash(password, 10, (hashError, hash) => {
         if (hashError) {
-            return res.status(500).json({
+            return res.status(401).json({
                 message: hashError.message,
                 error: hashError
             });
@@ -25,17 +29,82 @@ const register = (req: Request, res: Response, next: NextFunction) => {
 
         // @TODO: insert user into DB
 
-        // let query = `INSERT INTO users (username, password) VALUES ("${username}", "${hash}")`;
-        // let result = Query(query);        
+        const query = `INSERT INTO users (username, password)
+                        VALUES ('${username}', '${hash}')
+                        RETURNING _id;`;
+
+        Query(query)
+            .then(result => {
+                console.log('RESULT:' + result);
+
+                // @TODO: create JWT Tokens & send with response? or redirect to login?
+
+                return res.status(201).json({ id: result.rows[0]._id });
+            })
+            .catch(error => {
+                console.log(`Login: ${error.message, error}`)
+
+                return res.status(500).json({
+                    message: error.message,
+                    error
+                });
+            });
     });
 }
 
-const login = (req: Request, res: Response, next: NextFunction) => {
+const login = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
     
+    let { username, password } = req.body;
+
+    let query = `SELECT * FROM users WHERE username = '${username}'`;
+
+    Query(query)
+        .then(async result => {
+            const passMatch = await bcryptjs.compare(password, result.rows[0].password);
+
+            if (passMatch) {
+                console.log("Login: passwords match");
+
+                // @TODO: create JWT Tokens & send with response
+
+                return res.status(201).json({ 
+                    message: "Login successful",
+                    id: result.rows[0]._id
+                });
+            } else {
+                console.error("Login: Incorrect password");
+
+                return res.status(401).json({
+                    message: "Incorrect password",
+                });
+            }
+        })
+        .catch(error => {
+            console.log(`Login: ${error.message, error}`)
+
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
 }
 
 const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
-    
+    Query("SELECT * FROM users")
+        .then(result => {
+            console.log('Retrieved users from database.', result.rows)
+
+            return res.status(201).json(result.rows);
+        })
+        .catch(error => {
+            console.error(`Get All Users: ${error.message, error}`)
+
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
 }
 
 export default { validateToken, register, login, getAllUsers };
--- src/lifelog-api.ts
+++ src/lifelog-api.ts
@@ -1,18 +1,22 @@
+import bodyParser from "body-parser";
 import express, { Application, Request, Response } from "express";
 import { Query } from './config/psql';
+import userRoutes from './routes/user';
 
 const app: Application = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('<h2>The API for lifelog.lennythedude.com will be here shortly</h2>');
 });
 
-Query("SELECT * FROM users")
-    .then(result => console.log('Query result:', result.rows))
-    .catch(error => console.error('Error executing query', error.stack));
+// app.post('/register', register)
 
+app.use('/users', userRoutes);
 
 app.listen(port, () => {
     return console.log(`The Lifelog API is listening on port ${port}`);
-})
+});

a0a14223fc6d81b2d449f8b2c9bf3a6b9b3b9a09
Leo A <lennythedude27@gmail.com>
2023-03-31 21:12:33 +0530

    Added cors

--- src/lifelog-api.ts
+++ src/lifelog-api.ts
@@ -2,12 +2,15 @@ import bodyParser from "body-parser";
 import express, { Application, Request, Response } from "express";
 import userRoutes from './routes/user';
 import config from "./config/config";
+import cors from "cors";
 
 const app: Application = express();
 const port = config.server.port || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({ origin: "*" }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('<h2>The API for lifelog.lennythedude.com will be here shortly</h2>');
